4c58d77c68e4fc4bfd49feff691a784e05fef652
RodyaPik <116595792+RodyaPik@users.noreply.github.com>
2023-05-15 13:19:15 +0400

    Update and rename Catalog.java to Brand.java

--- Catalog.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Catalog extends Laptop {
-
-    public Catalog(int id, int ram, int ssd, String os, String color){
-        super(id,ram,ssd,os,color);
-    }
- 
-}
